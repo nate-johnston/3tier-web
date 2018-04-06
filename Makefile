89184a92015026e6b97a93a669f725c028e91b3d
Nate Johnston <nate_johnston@cable.comcast.com>
2018-04-06 13:13:41 -0400

    [1.0.7] fix URL for app server

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME=3tier-web
-VERSION=1.0.6
+VERSION=1.0.7
 REVISION=0
 PREFIX=/app/threetier-web
 TARGET_DIR=.
--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('http://threetier-prod-app-1693303267.us-east-2.elb.amazonaws.com/').
+    $http.get('http://threetier-prod-app-1693303267.us-east-2.elb.amazonaws.com/users').
         then(function(response) {
             $scope.greeting = response.data;
         });
